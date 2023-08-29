01b481954260641fd44b24c4348dba8483f942a1
handuah <handuah@gmail.com>
2023-08-29 12:22:37 +0000

    changed api link

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,10 +1,18 @@
+// // ignore: constant_identifier_names
+// const String base_url = 'https://api.weatherbit.io/v2.0/';
+
+// // ignore: non_constant_identifier_names
+// const String API_KEY = '1dca4687b313448bb8c162345b0e5f13';
+
+// // Example Call
+// // https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=1dca4687b313448bb8c162345b0e5f13&include=minutely
+
+// //https://api.weatherbit.io/v2.0/current?city=Accra&country=Ghana&key=1dca4687b313448bb8c162345b0e5f13
+
+/////////////////////////
+// USING WEATHERAPI.COM//
 // ignore: constant_identifier_names
-const String base_url = 'https://api.weatherbit.io/v2.0/';
+const String base_url = 'https://api.weatherapi.com/v1/';
 
 // ignore: non_constant_identifier_names
-const String API_KEY = '1dca4687b313448bb8c162345b0e5f13';
-
-// Example Call
-// https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=1dca4687b313448bb8c162345b0e5f13&include=minutely
-
-//https://api.weatherbit.io/v2.0/current?city=Accra&country=Ghana&key=1dca4687b313448bb8c162345b0e5f13
+const String API_KEY = 'dfce69c8d77f4714b29121448232908';
