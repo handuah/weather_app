dc7d1b42577707d90d171aa753faa76e00fc9cd8
handuah <handuah@gmail.com>
2023-09-05 14:08:39 +0000

    made some changes to background color etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:weather_app/screens/main_page.dart';
 
 void main() {
   runApp(const MyApp());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    // systemNavigationBarColor: Colors.amber.withOpacity(0.6),
+    statusBarColor: Colors.transparent,
+  ));
 }
 
 class MyApp extends StatelessWidget {
